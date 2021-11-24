23c02689a805f279aced42b43a8f43461b5f651e
Peter Sefton <pt@ptsefton.com>
2021-11-24 15:36:55 +1100

    Added some higher-level tools

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib/ocfl-tools');
+
+
+module.exports = {
+    ocfltools: require('./lib/ocfl-tools'),
+    Collector: require('./lib/collector'),
+    ROCrate:   require('./lib/ro-crate-extensions'),
+    mintArcpId: require('./lib/mint-arcp-id')
+}
