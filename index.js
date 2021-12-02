406f91e71b8ecc47b4dea0c9568dd3093fd3ff84
Peter Sefton <pt@ptsefton.com>
2021-12-02 13:41:06 +1100

    Refactoring corpus tools

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = {
     ocfltools: require('./lib/ocfl-tools'),
     Collector: require('./lib/collector'),
     ROCrate:   require('./lib/ro-crate-extensions'),
+    Provenance: require('./lib/provenance'),
     mintArcpId: require('./lib/mint-arcp-id')
 }
