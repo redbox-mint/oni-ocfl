864896ca876f94e862409261debe5778b5a20647
Moises Sacal <moisbo@gmail.com>
2022-03-22 14:45:45 +1100

    incorporating both new ro-crate and new ocfl-js library

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 module.exports = {
     ocfltools: require('./lib/ocfl-tools'),
     Collector: require('./lib/collector'),
-    ROCrate:   require('./lib/ro-crate-extensions'),
     Provenance: require('./lib/provenance'),
     mintArcpId: require('./lib/mint-arcp-id')
 }
