8b9f017471a6464a33ac05ccd414ab4245adc932
Moises Sacal <moisbo@gmail.com>
2022-03-24 13:38:05 +1100

    changed arcpId in collector to use static method

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ module.exports = {
     ocfltools: require('./lib/ocfl-tools'),
     Collector: require('./lib/collector'),
     Provenance: require('./lib/provenance'),
-    mintArcpId: require('./lib/mint-arcp-id')
+    generateArcpId: require('./lib/mint-arcp-id')
 }
